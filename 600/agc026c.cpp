5c588842d304d272c9cd8d13ce714b7f686c30a6
kagasan <ghghkaja@gmail.com>
2019-04-15 09:28:45 +0900

    なんかサンプル合わない

--- 600/agc026c.cpp
+++ 600/agc026c.cpp
@@ -1,23 +1,64 @@
-#include "bits/stdc++.h"
+#include <iostream>
+#include <string>
+#include <random>
+#include <map>
 using namespace std;
 typedef long long ll;
 
 int main(){
+
+    ll rnd[18][26];
+    random_device rd;
+    mt19937_64 mt(rd());
+    for(int i = 0; i < 18; i++){
+        for(int j = 0; j < 26; j++){
+            rnd[i][j] = mt();
+        }
+    }
 
     ll N;
     cin >> N;
     string s;
     cin >> s;
 
-    // 現れる全ての文字は偶数個必要
-    map<char, int>m;
-    for(int i = 0; i < s.size(); i++)m[s[i]]++;
-    for(int i = 0; i < s.size(); i++){
-        if(m[s[i]] & 1){
-            cout << 0 << endl;
-            return 0;
+    ll ans = 0;
+    map<ll, ll>m;
+    for(ll i = 0; i < (1 << N); i++){
+        ll hs = 0;
+        ll idx = 0;
+        for(ll j = 1, k = 0; k < N; j <<= 1, k++){
+            if((i & j) == 0){
+                hs ^= rnd[idx][s[k] - 'a'];
+                idx++;
+            }
         }
+        for(ll j = (1 << (N - 1)), k = N - 1; k >= 0; k--){
+            if(i & j){
+                hs ^= rnd[idx][s[k] - 'a'];
+                idx++;
+            }
+        }
+        m[hs]++;
     }
+    for(ll i = 0; i < (1 << N); i++){
+        ll hs = 0;
+        ll idx = 0;
+        for(ll j = 1, k = 0; k < N; j <<= 1, k++){
+            if((i & j) == 0){
+                hs ^= rnd[idx][s[k + N] - 'a'];
+                idx++;
+            }
+        }
+        for(ll j = (1 << (N - 1)), k = N - 1; k >= 0; k--){
+            if(i & j){
+                hs ^= rnd[idx][s[k + N] - 'a'];
+                idx++;
+            }
+        }
+        ans += m[hs];
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
