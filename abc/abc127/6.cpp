6914f08be3f47b4cacb76507b07dceb9e7350c0e
kagasan <ghghkaja@gmail.com>
2019-05-26 17:59:18 +0900

    ABC 127 F

--- abc/abc127/6.cpp
+++ abc/abc127/6.cpp
@@ -2,57 +2,37 @@
 using namespace std;
 typedef long long ll;
 
-priority_queue<ll>X;
-priority_queue<ll, vector<ll>, greater<ll> >Y;
+priority_queue<ll>Q1;
+priority_queue<ll, vector<ll>, greater<ll> >Q2;
 
-void push(ll x){
-    X.push(x);
-    while(Y.size() > X.size()){
-        ll tmp = Y.top();
-        Y.pop();
-        X.push(tmp);
-    }
-    while(Y.size() + 2 <= X.size()){
-        ll tmp = X.top();
-        X.pop();
-        Y.push(tmp);
-    }
-}
-
-ll mid(){
-    if(X.empty())return 0;
-    return X.top();
-}
-
-vector<ll>A;
-ll B = 0;
-ll f(ll x){
-    ll res = B;
-    for(ll i = 0; i < A.size(); i++){
-        res += abs(x - A[i]);
-    }
-    return res;
+ll push(ll x){
+    Q1.push(x);
+    Q2.push(x);
+    ll q1 = Q1.top();
+    Q1.pop();
+    ll q2 = Q2.top();
+    Q2.pop();
+    Q1.push(min(q1, q2));
+    Q2.push(max(q1, q2));
+    return abs(q1 - q2);
 }
 
 int main(){
     
-    
-
     ll Q;
     cin >> Q;
+    ll x = 0;
+    ll fx = 0;
     for(ll i = 0; i < Q; i++){
         ll q;
         cin >> q;
         if(q == 1){
             ll a, b;
             cin >> a >> b;
-            A.push_back(a);
-            push(a);
-            B += b;
+            fx += b + push(a);
         }
         else{
-            ll x = mid();
-            cout << x << " " << f(x) << endl;
+            cout << Q1.top() << " " << fx << endl;
         }
     }
 
