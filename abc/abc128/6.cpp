38f9a3d566be6c29ee7d14ccb0c780723c9116ce
kagasan <ghghkaja@gmail.com>
2019-05-28 12:14:12 +0900

    ABC 128 F

--- abc/abc128/6.cpp
+++ abc/abc128/6.cpp
@@ -1,40 +1,22 @@
 #include "bits/stdc++.h"
 using namespace std;
 typedef long long ll;
-
-priority_queue<ll>Q1;
-priority_queue<ll, vector<ll>, greater<ll> >Q2;
-
-ll push(ll x){
-    Q1.push(x);
-    Q2.push(x);
-    ll q1 = Q1.top();
-    Q1.pop();
-    ll q2 = Q2.top();
-    Q2.pop();
-    Q1.push(min(q1, q2));
-    Q2.push(max(q1, q2));
-    return abs(q1 - q2);
-}
-
 int main(){
-    
-    ll Q;
-    cin >> Q;
-    ll x = 0;
-    ll fx = 0;
-    for(ll i = 0; i < Q; i++){
-        ll q;
-        cin >> q;
-        if(q == 1){
-            ll a, b;
-            cin >> a >> b;
-            fx += b + push(a);
-        }
-        else{
-            cout << Q1.top() << " " << fx << endl;
+    ll N;
+    cin >> N;
+    vector<ll>v(N);
+    for(ll i = 0; i < N; i++)cin >> v[i];
+    ll ans = 0;
+    for(ll d = 1; d < N; d++){
+        ll score = 0;
+        for(ll j = 0; j < N - d; j += d){
+            score += v[j] + v[N - 1 - j];
+            if((N - 1) % d == 0 && j >= N - 1 - j){
+                break;
+            }
+            ans = max(ans, score);
         }
     }
-
+    cout << ans << endl;
     return 0;
 }
