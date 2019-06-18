0c26188c3382012ca3799823e00afcbbb2e5030d
kagasan <ghghkaja@gmail.com>
2019-06-18 23:50:16 +0900

    テンプレ強化

--- template/1.cpp
+++ template/1.cpp
@@ -1,8 +1,12 @@
 #include "bits/stdc++.h"
 using namespace std;
+#define all(x) (x).begin(), (x).end()
 typedef long long ll;
-typedef vector<ll> vll;
-typedef vector<vll> vvll;
+typedef pair<ll, ll>P;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+void SAY(bool YES){cout<<(YES?"YES":"NO")<<endl;};
+void Say(bool Yes){cout<<(Yes?"Yes":"No")<<endl;};
 
 int main(){
 
