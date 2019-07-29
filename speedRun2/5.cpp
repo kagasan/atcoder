8c83fad874094dc463ead1f7ad4857aa6a3637e6
kagasan <ghghkaja@gmail.com>
2019-07-29 21:49:35 +0900

    speedrun 2-5

---
+++ speedRun2/5.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+void Say(bool say, string a = "Yes", string b = "No"){cout << (say ? a : b) << endl;};
+
+int main(){
+    ll N;
+    cin >> N;
+    vector<ll>A(N + 1, 0), B(N + 1, 0);
+    for(ll i = 1; i <= N; i++)cin >> A[i] >> B[i];
+    ll ans = 0;
+    for(ll i = 1; i <= N; i++)ans += min(A[i] / 2, B[i]);
+    cout << ans << endl;
+
+    return 0;
+}
