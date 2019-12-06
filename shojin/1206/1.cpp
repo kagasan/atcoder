f165e96efc5b3275e2c76f3be0d7c06dcfa24a32
kagasan <ghghkaja@gmail.com>
2019-12-06 19:02:54 +0900

    shojin cf2016 a

---
+++ shojin/1206/1.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for(ll (i) = 0; (i) < (n); (i)++)
+#define rep1(i, n) for(ll (i) = 1; (i) <= (n); (i)++)
+#define rrep(i, n) for(ll (i) = (n) - 1; (i) >= 0; (i)--)
+#define rrep1(i, n) for(ll (i) = (n); (i) >= 1; (i)--)
+const ll INF = 1145141919;
+const ll MOD = 1000000007;
+template<class T> void chmax(T &a, const T &b){if(a < b){a = b;}}
+template<class T> void chmin(T &a, const T &b){if(a > b){a = b;}}
+
+int main(){
+
+    ll H, W;
+    cin >> H >> W;
+    rep(y, H)rep(x, W){
+        string s;
+        cin >> s;
+        if(s == "snuke"){
+            cout << (char)(x + 'A') << y + 1 << endl;
+        }
+    }
+
+    return 0;
+}
