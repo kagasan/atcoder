3e5231996ee16a6515d0b81eccdd49dc7235d815
kagasan <ghghkaja@gmail.com>
2019-12-22 00:42:27 +0900

    shojin 1222

---
+++ shojin/1222/6.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for(ll (i) = 0; (i) < (n); (i)++)
+#define rep1(i, n) for(ll (i) = 1; (i) <= (n); (i)++)
+#define rrep(i, n) for(ll (i) = (n) - 1; (i) >= 0; (i)--)
+#define rrep1(i, n) for(ll (i) = (n); (i) >= 1; (i)--)
+const ll INF = 1145141919;
+const ll MOD = 1000000007;
+template<class T> void chmax(T &a, const T &b){if(a < b){a = b;}}
+template<class T> void chmin(T &a, const T &b){if(a > b){a = b;}}
+
+int main(){
+
+    string s, ans = "No";
+    cin >> s;
+    rep(i, s.size() - 1)if(s[i] == 'A' && s[i + 1] == 'C')ans = "Yes";
+
+    cout << ans << endl;
+    
+    return 0;
+}
